cd668ca199e3840d019393bfa76ed88029202c01
Slobodanka Jovicic <sjovicic@biomech.us>
2024-03-07 15:57:03 +0100

    Update ImageUpload.tsx

--- coretex-web-app-demo/src/pages/ImageUpload/ImageUpload.tsx
+++ coretex-web-app-demo/src/pages/ImageUpload/ImageUpload.tsx
@@ -109,7 +109,6 @@ const ImageUpload: FC<ImageUploadProps> = ({ refreshToken, apiServerURL }) => {
 
   useEffect(() => {
     (async () => {
-      console.log(navigator);
       if (navigator && navigator.mediaDevices) {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const videoDevices = devices.filter((i) => i.kind === "videoinput");
@@ -117,8 +116,6 @@ const ImageUpload: FC<ImageUploadProps> = ({ refreshToken, apiServerURL }) => {
       }
     })();
   }, []);
-
-  console.log(devices);
 
   return (
     <>
