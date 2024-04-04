95c318c6b849ebecd292d064c5f92a70675dc4a9
dpavloviic <161023752+dpavloviic@users.noreply.github.com>
2024-04-04 10:56:50 +0200

    Update ImageUpload.tsx

--- coretex-web-app-demo/src/pages/ImageUpload/ImageUpload.tsx
+++ coretex-web-app-demo/src/pages/ImageUpload/ImageUpload.tsx
@@ -38,7 +38,7 @@ const ImageUpload: FC<ImageUploadProps> = ({ refreshToken, apiServerURL }) => {
   const webcamRef = useRef<any>(null);
   const webcamWrapperRef = useRef<HTMLDivElement | null>(null);
 
-  const [modelID, setModelID] = useState<number>(171);
+  const [modelID, setModelID] = useState<number>(173);
   const [nodeID, setNodeID] = useState<number>(317);
   const [response, setResponse] = useState<string>("");
   const [debugData, setDebugData] = useState<any>();
